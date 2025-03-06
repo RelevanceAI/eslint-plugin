24a417e3910cdc85c7d662cab800509ded770dc2
Soorria Saruva <soorria.ss@gmail.com>
2025-03-06 17:43:47 +1100

    fix: make expect and vitest rule work more

---
+++ .changeset/tall-bananas-cheer.md
@@ -0,0 +1,5 @@
+---
+'@relevanceai/eslint-plugin': minor
+---
+
+fix: make expect and vitest rule work more
--- src/rules/no-vitest-and-expect-package.js
+++ src/rules/no-vitest-and-expect-package.js
@@ -9,18 +9,27 @@ const rule = {
 
   create: function create(context) {
     let hasVitestImport = false
+    /**
+     * @type {import('eslint').Rule.Node | null}
+     */
+    let expectImportNode = null
     return {
+      'Program:exit'() {
+        if (expectImportNode && hasVitestImport) {
+          context.report({
+            node: expectImportNode,
+            message: "Don't import from 'expect' when using 'vitest' ya dingus",
+          })
+        }
+      },
       ImportDeclaration(node) {
         if (node.source.value === 'vitest') {
           hasVitestImport = true
         }
       },
       'ImportDeclaration:exit'(node) {
-        if (node.source.value === 'expect' && hasVitestImport) {
-          context.report({
-            node,
-            message: "Don't import from 'expect' when using 'vitest' ya dingus",
-          })
+        if (node.source.value === 'expect') {
+          expectImportNode = node
         }
       },
     }
