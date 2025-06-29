075483aa26bb9a3034a87819c2e6560b69abd56d
Campbell Wass <campbell.wass@gmail.com>
2025-06-29 18:38:10 +1000

    chore: documentation

--- src/rules/pascal-case-functions-and-types.js
+++ src/rules/pascal-case-functions-and-types.js
@@ -1,8 +1,99 @@
+/*
+
+This rule looks at each variable and tries to determine if it's a
+function/callable. If it is, then the variable is expected to have a PascalCase
+name.
+
+The rule was written to replace @typescript-eslint/naming-convention for our
+needs, since it was taking up about 25 seconds, or about 40%, of the time taken
+to lint one of our repos.
+
+This rule is faster because it doesn't need to use TypeScript to calculate the
+type of every variable and check if it's a function - we just use syntactic
+analysis and some hardcoded special cases based on the functions used in our
+repo.
+
+The hardcoded cases for Vitest are:
+  - methods that are only called on mocked functions:
+    -mockResolvedValue
+    -mockResolvedValueOnce
+    -mockReturnValue
+    -mockReturnValueOnce
+    -mockImplementation
+    -mockImplementationOnce
+    -mockRejectedValue
+    -mockRejectedValueOnce
+    -mockReturnThis
+  - vi.fn()
+  - vi.mocked().X() where X is one of the methods that are only called on mocked
+    functions.
+  - expect(foo).X(), where X is one of the methods that are only called on
+    expect for functions:
+    - toHaveBeenCalledTimes
+    - toHaveBeenCalledWith
+    - toHaveBeenCalledOnce
+
+We use these values to infer that a variable is a function even we we don't see
+a function being assigned to a variable.
+
+The test for PascalCase is quite primitive/liberal (borrowed from
+@typescript-eslint/naming-convention), and we intentionally don't generate fixes
+for the naming violations, since that would require analysing which parts of a
+variable are words/acronyms.
+
+In the following cases, we use these definitions/meanings:
+- RHS = right hand side = "the thing assigned to a variable".
+- MemberExpression is property access, and we only consider non-computed
+  MemberExpressions.
+
+Assignment based cases:
+Case 1: Variable has a function assigned to it, or is declared with a function
+as the initial value.
+  a. RHS is arrow function or function expression:
+    const foo = () => {};
+    const foo = function() {};
+
+  b. RHS is a call to a known function factory (function that returns functions):
+    const foo = KnownFunctionFactory();
+
+  c. RHS is a MemberExpression and the property name is PascalCase (indicating
+  that it's a function in our convention):
+    const foo = someObject.SomeFunction;
+
+  d. RHS is a CallExpression, optionally with member function calls chained onto
+  it if the methods are known Vitest function mocking methods.
+  The callee must be either:
+    1. A MemberExpression of the form `vi.fn()`, with optionally one argument:
+      const foo = vi.fn();
+      const foo = vi.fn(() => {});
+      const foo = vi.fn().mockResolvedValue().mockRejectedValue();
+
+    2. A MemberExpression of the form `vi.mocked(args)`, where args is either
+    a PascalCase identifier, or a MemberExpression with a PascalCase property:
+      const foo = vi.mocked(SomeFunction);
+      const foo = vi.mocked(obj.SomeFunction);
+
+Usage based cases:
+Case 2:
+  a. The variable is called:
+    const foo = blah; foo();
+  b. The variable has a known Vitest function mocking method called on it:
+    const foo = blah; foo.mockResolvedValue();
+  c. The variable is passed as the sole argument to Vitest's `expect` function,
+  which then has a known Vitest expect method for functions:
+    const foo = blah; expect(foo).toHaveBeenCalledTimes();
+
+*/
+
 /**
  * @type {import('eslint').Rule.RuleModule}
  */
 module.exports = {
   meta: {
+    docs: {
+      description:
+        'Ensure all variables that contain functions have PascalCase names.',
+    },
     type: 'problem',
     schema: [
       {
@@ -32,6 +123,7 @@ module.exports = {
 
     return {
       VariableDeclarator(node) {
+        // Case 1. for declarations.
         if (node.id.type === 'Identifier') {
           if (node.init && isRHSFunction(node.init, knownFunctionFactories)) {
             const name = node.id.name
@@ -71,6 +163,7 @@ function checkVariable(
   references,
   knownFunctionFactories,
 ) {
+  // Case 2.a.
   const isCalled = references.some((r) => {
     const parent = r.identifier.parent
     return parent.type === 'CallExpression' && parent.callee === r.identifier
@@ -80,6 +173,7 @@ function checkVariable(
     checkAndReport(context, bindingIdentifiers, name)
   }
 
+  // Case 1. for assignments.
   const isAssignedAFunction = references.some((r) => {
     const parent = r.identifier.parent
     if (
@@ -96,8 +190,7 @@ function checkVariable(
     checkAndReport(context, bindingIdentifiers, name)
   }
 
-  // const something = vi.mocked(...);
-  // something.mockImplementation(...);
+  // Case 2.b.
   const hasKnownViTestFunctionMockCall = references.some((r) => {
     const parent = r.identifier.parent
     if (
@@ -128,6 +221,7 @@ function checkVariable(
     checkAndReport(context, bindingIdentifiers, name)
   }
 
+  // Case 2.c.
   const usedInExpectToHaveBeenCalledTimes = references.some((r) => {
     const parent = r.identifier.parent
     if (
@@ -166,6 +260,7 @@ function checkVariable(
  * @param {Set<string> | undefined} knownFunctionFactories
  */
 function isRHSFunction(rhs, knownFunctionFactories) {
+  // Case 1.a.
   if (
     rhs.type === 'ArrowFunctionExpression' ||
     rhs.type === 'FunctionExpression'
@@ -173,6 +268,7 @@ function isRHSFunction(rhs, knownFunctionFactories) {
     return true
   }
 
+  // Case 1.b.
   if (
     rhs.type === 'CallExpression' &&
     rhs.callee.type === 'Identifier' &&
@@ -181,6 +277,7 @@ function isRHSFunction(rhs, knownFunctionFactories) {
     return true
   }
 
+  // Case 1.c.
   if (
     rhs.type === 'MemberExpression' &&
     !rhs.computed &&
@@ -190,19 +287,7 @@ function isRHSFunction(rhs, knownFunctionFactories) {
     return true
   }
 
-  /**
-   * @param {import("estree").CallExpression} call
-   */
-  function getLeftMostCall(call) {
-    while (
-      call.callee.type === 'MemberExpression' &&
-      call.callee.object.type === 'CallExpression'
-    ) {
-      call = call.callee.object
-    }
-    return call
-  }
-
+  // Case 1.d.
   if (rhs.type === 'CallExpression') {
     const leftMostCall = getLeftMostCall(rhs)
 
@@ -213,6 +298,15 @@ function isRHSFunction(rhs, knownFunctionFactories) {
       leftMostCall.callee.object.name === 'vi' &&
       leftMostCall.callee.property.type === 'Identifier'
     ) {
+      // Case 1.d.1.
+      if (
+        leftMostCall.callee.property.name === 'fn' &&
+        leftMostCall.arguments.length <= 1
+      ) {
+        return true
+      }
+
+      // Case 1.d.2.
       if (
         leftMostCall.callee.property.name === 'mocked' &&
         leftMostCall.arguments.length === 1
@@ -232,17 +326,23 @@ function isRHSFunction(rhs, knownFunctionFactories) {
           return true
         }
       }
-
-      if (
-        leftMostCall.callee.property.name === 'fn' &&
-        leftMostCall.arguments.length <= 1
-      ) {
-        return true
-      }
     }
   }
 
   return false
+}
+
+/**
+ * @param {import("estree").CallExpression} call
+ */
+function getLeftMostCall(call) {
+  while (
+    call.callee.type === 'MemberExpression' &&
+    call.callee.object.type === 'CallExpression'
+  ) {
+    call = call.callee.object
+  }
+  return call
 }
 
 /**
